23514c1e35fad56529439a827d91656338f4d802
AskilTheGinger <askilab@outlook.com>
2025-02-07 15:23:57 +0100

    tried to cook

--- frontend/JS/script.js
+++ frontend/JS/script.js
@@ -6,13 +6,29 @@ let colors = [
     "_of_spades.png",
     "_of_clubs.png",
     "_of_hearts.png" ]
+// assigns each card with a value
+    function assignValue(item, index){
+    if(index == 1){
+        item.dataset.value = 11
+    } else if (index > 10){
+        item.dataset.value = 10
+    } else{
+        item.dataset.value = index
+    }
+    console.log(item.dataset, item)
+}
+
 for (let c = 0; c < colors.length; c++) {
     const element = colors[c];
     for (let i = 1; i < 14; i++) {
             const card =document.createElement('img')
             let cardSrc = "bilder/kortstokk/"+i + element  
+            assignValue(cardSrc, i)
             fullDeck.push(cardSrc)
+            
             card.classList.add("kort")
+
+
         }
 }
 //lager en kopi av hele decket som skal brukes senere
@@ -37,12 +53,17 @@ function shuffle(array) {
     }
 }
 shuffle(deck)
-for (let i = 0; i < deck.length; i++) {
+
+handValue = 0
+for (let i = 0; i < 2; i++) {
     const element = deck[i];
     const card =document.createElement('img')
     card.src = element
     card.classList.add("kort")
     hand.appendChild(card)
+    handValue += card.dataset.value
+    console.log(handValue)
+    document.getElementById("winnerValue")
 }
 
 console.log(fullDeck)
